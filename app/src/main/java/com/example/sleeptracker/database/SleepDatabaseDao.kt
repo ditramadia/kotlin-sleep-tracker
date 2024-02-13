e5bdf7310db9f5eb445ecdd595c119b3d3a179fd
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-13 08:37:18 +0700

    feat: add database entity

--- app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
@@ -1,4 +1,30 @@
 package com.example.sleeptracker.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface SleepDatabaseDao {
+    @Insert
+    fun insertOne(night: SleepNight)
+
+    @Update
+    fun updateOne(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun getOne(key: Long) : SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAll(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
 }
