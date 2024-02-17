6f75ff1a2e87e67a176c8aca16623af6056d7ad0
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-17 23:04:02 +0700

    update: refactor on bind method

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.sleeptracker.sleeptracker
 
+import android.content.res.Resources
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -24,19 +25,7 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.sleepQuality.text = convertNumericQualityToString(item.sleepQuality, res)
-
-        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -49,5 +38,21 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         val sleepLength : TextView = itemView.findViewById(R.id.sleep_length)
         val sleepQuality : TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage : ImageView = itemView.findViewById(R.id.quality_image)
+
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            sleepQuality.text = convertNumericQualityToString(item.sleepQuality, res)
+
+            qualityImage.setImageResource(when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            })
+        }
     }
 }
